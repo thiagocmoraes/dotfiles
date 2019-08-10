ce6874f7aecbe0014e054d15db6641b2a891dd5a
Thiago Moraes <thiago@cmoraes.com>
2019-08-10 14:31:09 -0700

    Minimal changes to keep up with time

--- cask.sh
+++ cask.sh
@@ -12,13 +12,11 @@ brew cask install flux 2> /dev/null
 brew cask install google-chrome 2> /dev/null
 brew cask install google-hangouts 2> /dev/null
 brew cask install iterm2 2> /dev/null
-brew cask install macvim 2> /dev/null
 brew cask install sublime-text3 2> /dev/null
 brew cask install the-unarchiver 2> /dev/null
 brew cask install transmission 2> /dev/null
 brew cask install ukelele 2> /dev/null
 brew cask install virtualbox 2> /dev/null
 brew cask install vlc 2> /dev/null
-brew cask install notational-velocity 2> /dev/null
 
 brew cask install font-source-code-pro 2> /dev/null
